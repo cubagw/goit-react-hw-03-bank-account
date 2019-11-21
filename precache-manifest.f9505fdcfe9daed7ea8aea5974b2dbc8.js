f0616f4bb40d2d53fe8833c68da75217207705b3
dmytro vitenko <cubagw84@gmail.com>
2019-11-21 11:07:03 +0200

    Updates

---
+++ precache-manifest.f9505fdcfe9daed7ea8aea5974b2dbc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5f8ddd86624661ebd43929b4d310b0",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "facf3baa09e6b214bc28",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "99e5f79c9b9f44042adb",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.201504da.chunk.css"
+  },
+  {
+    "revision": "facf3baa09e6b214bc28",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.7e4c6cb8.chunk.js"
+  },
+  {
+    "revision": "99e5f79c9b9f44042adb",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.5345557d.chunk.js"
+  },
+  {
+    "revision": "a248bbf272ad9faaad52",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.e6999974.js"
+  }
+]);
